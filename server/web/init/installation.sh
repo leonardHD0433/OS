74cec50dd2cb87b54019db5dd0ddd1f381659882
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-28 06:45:55 +0800

    changes

--- server/web/init/installation.sh
+++ server/web/init/installation.sh
@@ -15,7 +15,7 @@ dnf install -y php-fpm
 dnf install -y tar
 dnf clean all
 
-cp ./web/tar/latest.tar.gz /var/www/latest.tar.gz
+cp /web/tar/latest.tar.gz /var/www/latest.tar.gz
 tar zxvf /var/www/latest.tar.gz -C /var/www/
 chown -R apache. /var/www/wordpress
 cp /web/conf/httpd.conf /etc/httpd/conf/httpd.conf

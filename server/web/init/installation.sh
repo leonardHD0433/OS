1871fa00bce3a8b121433b66da70db13a6ea316d
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-28 07:11:21 +0800

    changes

--- server/web/init/installation.sh
+++ server/web/init/installation.sh
@@ -22,7 +22,14 @@ cp /web/conf/httpd.conf /etc/httpd/conf/httpd.conf
 cp /web/conf/www.conf /etc/php-fpm.d/www.conf
 cp /web/conf/wordpress.conf /etc/httpd/conf.d/wordpress.conf
 rm /etc/httpd/conf.d/ssl.conf
-#cp /web/conf/192.168.1.4-ssl.conf /etc/httpd/conf.d/192.168.1.4-ssl.conf
+cp /web/conf/192.168.1.4-ssl.conf /etc/httpd/conf.d/192.168.1.4-ssl.conf
+# Generate SSL certificate and key
+cd /etc/pki/tls/certs
+openssl genrsa -aes128 -passout pass:passwd -out server.key 2048
+openssl rsa -in server.key -passin pass:passwd -out server.key
+openssl req -utf8 -new -key server.key -out server.csr -subj "/C=MY/ST=Selangor/L=Shah Alam/O=UOW Malaysia/OU=Operating Systems/CN=pwe69.com/emailAddress=root@pwe69.com"
+openssl x509 -in server.csr -out server.crt -req -signkey server.key -days 3650
+sudo chmod 600 server.key
 
 # Test Apache configuration
 apachectl configtest

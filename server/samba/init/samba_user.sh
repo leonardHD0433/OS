3e34d5a712e4694d50b9db7b3cff428905e084a5
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-27 17:22:05 +0800

    changes

--- server/samba/init/samba_user.sh
+++ server/samba/init/samba_user.sh
@@ -2,7 +2,9 @@
 
 # Add a new group
 groupadd $SMB_GROUP
-mkdir $SMB_DIR
+
+# Create the shared directory and set permissions
+mkdir -p $SMB_DIR
 chmod 770 $SMB_DIR
 
 # Add a new user and assign it to the group
@@ -10,19 +12,12 @@ useradd -m $SMB_USER
 echo -e "$SMB_PASS\n$SMB_PASS" | smbpasswd -a -s $SMB_USER
 usermod -aG $SMB_GROUP $SMB_USER
 
-chcon -R -t samba_share_t /home/shareRecord
-semanage fcontext -a -t samba_share_t "/home/shareRecord(/.*)?"
-restorecon -Rv /home/shareRecord
-load_policy
-
-# Restart the Samba service
-service smb restart
-service nmb restart
+# Start the Samba service
+smbd
+nmbd
 
 # Verify Samba configuration
 testparm -s
 
-echo smbpasswd -a $SMB_USER
-
 # Start the Samba server
-exec /usr/bin/samba.sh
+exec /usr/sbin/smbd -FS

cb91253eca0d74c2eeb69947eb2e750098eed4d6
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-27 15:17:35 +0800

    changes

--- install_docker.sh
+++ install_docker.sh
@@ -5,6 +5,8 @@ dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 sudo dnf install -y python3-pip
 dnf install docker-ce docker-ce-cli containerd.io -y
 
+sudo dnf install docker-compose-plugin -y
+
 dnf install -y python3-pip
 
 pip3 --version

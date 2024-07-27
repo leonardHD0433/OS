1081efc832268b46d93ab2a2b626a2debaf35e65
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-27 18:12:38 +0800

    changes

--- server/samba/init/samba_user.sh
+++ server/samba/init/samba_user.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Function to log messages
 log() {
@@ -9,7 +9,7 @@ log "Starting Samba setup script..."
 
 # Add a new group
 log "Adding group: $SMB_GROUP"
-addgroup $SMB_GROUP || { log "Failed to add group $SMB_GROUP"; exit 1; }
+groupadd $SMB_GROUP || { log "Failed to add group $SMB_GROUP"; exit 1; }
 
 # Create the shared directory and set permissions
 log "Creating shared directory: $SMB_DIR"
@@ -18,7 +18,7 @@ chmod 770 $SMB_DIR || { log "Failed to set permissions on $SMB_DIR"; exit 1; }
 
 # Add a new user and assign it to the group
 log "Adding user: $SMB_USER"
-adduser -m $SMB_USER || { log "Failed to add user $SMB_USER"; exit 1; }
+useradd -m $SMB_USER || { log "Failed to add user $SMB_USER"; exit 1; }
 echo -e "$SMB_PASS\n$SMB_PASS" | smbpasswd -a -s $SMB_USER || { log "Failed to set Samba password for $SMB_USER"; exit 1; }
 usermod -aG $SMB_GROUP $SMB_USER || { log "Failed to add user $SMB_USER to group $SMB_GROUP"; exit 1; }
 chown -R $SMB_USER:$SMB_GROUP $SMB_DIR || { log "Failed to change ownership of $SMB_DIR"; exit 1; }

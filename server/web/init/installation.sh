42195821179c4b3667f097114ec6f2a3c08c5cb6
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-28 04:44:26 +0800

    cages

--- server/web/init/installation.sh
+++ server/web/init/installation.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
 dnf install -y httpd php-mysqlnd
-dnf clean all
 systemctl enable httpd
 rm /etc/httpd/conf.d/welcome.conf 
 
 dnf module reset php -y
 dnf module enable php:8.2 -y
 dnf module install php:8.2/common -y
+dnf --enablerepo=epel -y install php-pear php-mbstring php-pdo php-gd php-mysqlnd php-IDNA_Convert php-enchant enchant hunspell
+dnf install openssl -y
+dnf clean all
+
+mkdir -p /var/www/wordpress
+rmdir /var/www/html
+cp /web/conf/httpd.conf /etc/httpd/conf/httpd.conf
+cp /web/conf/www.conf /etc/php-fpm.d/www.conf
+cp /web/wordpress /var/www/wordpress
+chown -R apache. /var/www/wordpress
+cp /web/cert/server.crt /etc/pki/tls/certs/server.crt
+cp /web/cert/server.crt /etc/ssl/certs/server.crt
+cp /web/cert/server.csr /etc/pki/tls/certs/server.csr
+cp /web/cert/server.csr /etc/ssl/certs/server.csr
+cp /web/cert/server.key /etc/pki/tls/certs/server.key
+cp /web/cert/server.key /etc/ssl/certs/server.key
+cp /web/conf/192.168.1.4-ssl.conf /etc/httpd/conf.d/192.168.1.4-ssl.conf
+cp /web/conf/wordpress.conf /etc/httpd/conf.d/wordpress.conf
+chmod 600 /etc/pki/tls/certs/server.key
+
+# Test Apache configuration
+apachectl configtest
+
+service php-fpm start
+service httpd start

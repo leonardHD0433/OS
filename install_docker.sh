17b920cb3f7e29c25053286fe7f64e947e5279fa
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-27 15:28:47 +0800

    changes

--- install_docker.sh
+++ install_docker.sh
@@ -1,23 +1,14 @@
 #i will execute commands to run this script
 
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf config-manager --add-repo=https://download.docker.com/linux/rhel/docker-ce.repo
 
-sudo dnf install -y python3-pip
-dnf install docker-ce docker-ce-cli containerd.io -y
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo dnf install docker-compose-plugin -y
+sudo systemctl enable --now docker.service
 
-dnf install -y python3-pip
-
-pip3 --version
-
-pip3 install --upgrade docker-compose docker
-
-pip3 install --upgrade cryptography
+docker --version
 
 docker-compose --version
 
-systemctl enable docker
 
-systemctl start docker
 

e887aedd71b2cfe8988b597355f862500798b363
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-28 03:35:10 +0800

    changes

--- server/web/init/installation.sh
+++ server/web/init/installation.sh
@@ -3,3 +3,8 @@
 dnf install -y httpd php-mysqlnd
 dnf clean all
 systemctl enable httpd
+rm /etc/httpd/conf.d/welcome.conf 
+
+dnf module reset php -y
+dnf module enable php:8.2 -y
+dnf module install php:8.2/common -y

a39b66ed8ffb4d3a5a12eb26924254b5fef16f56
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-28 08:35:56 +0800

    changes

--- server/web/init/installation.sh
+++ server/web/init/installation.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Define paths
-CERT_DIR="/etc/pki/tls/certs"
-KEY_FILE="$CERT_DIR/server.key"
-CSR_FILE="$CERT_DIR/server.csr"
-CRT_FILE="$CERT_DIR/server.crt"
-
 dnf install -y httpd php-mysqlnd
 dnf install -y epel-release
 systemctl enable httpd
@@ -22,19 +16,4 @@ dnf install -y tar
 dnf clean all
 
 cp /web/tar/wordpress-6.6.1.tar.gz /var/www/wordpress-6.6.1.tar.gz
-cp /web/conf/httpd.conf /etc/httpd/conf/httpd.conf
-cp /web/conf/www.conf /etc/php-fpm.d/www.conf
-cp /web/conf/wordpress.conf /etc/httpd/conf.d/wordpress.conf
-rm /etc/httpd/conf.d/ssl.conf
 
-cp /web/cert/server.key $KEY_FILE
-cp /web/cert/server.csr $CSR_FILE
-cp /web/cert/server.crt $CRT_FILE
-
-cp $KEY_FILE /etc/ssl/certs/server.key
-cp $CRT_FILE /etc/ssl/certs/server.crt
-
-cp /web/conf/192.168.1.4-ssl.conf /etc/httpd/conf.d/192.168.1.4-ssl.conf
-
-# Test Apache configuration
-apachectl configtest

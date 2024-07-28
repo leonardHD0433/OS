1f963469a43740525bab7c084f96bc4d164df906
Edwin Chua Jin Rui <edwinchuajinrui@gmail.com>
2024-07-28 10:52:06 +0800

    changes

--- server/web/init/installation.sh
+++ server/web/init/installation.sh
@@ -10,6 +10,7 @@ dnf module enable php:8.2 -y
 dnf module install php:8.2/common -y
 dnf --enablerepo=epel -y install php-pear php-mbstring php-pdo php-gd php-mysqlnd php-IDNA_Convert php-enchant enchant hunspell
 dnf install openssl -y
+dnf install -y mariadb-client
 dnf install -y mod_ssl
 dnf install -y php-fpm
 dnf install -y tar
